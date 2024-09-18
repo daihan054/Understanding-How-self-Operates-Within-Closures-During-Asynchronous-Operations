84649cd1e8cb13df62eefb10e4f53ac805f8e7a2
revechatmobile <revechat.dev012@gmail.com>
2024-09-18 16:41:57 +0600

    first commit

---
+++ self understanding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  self understanding
+//
+//  Created by REVE Systems on 18/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
